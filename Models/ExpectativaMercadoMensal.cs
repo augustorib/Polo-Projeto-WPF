cd3fd13f744ca2ef04f0d0548f8afe7db84ec5e5
augustorib <augustorifra@gmail.com>
2024-06-27 01:44:47 -0300

    feat: Criação da ViewModel e Model  da expectativa do mercado mensal

---
+++ Models/ExpectativaMercadoMensal.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace Polo_Projeto_WPF.Models
+{
+    public class ExpectativaMercadoMensal
+    {
+        public string Indicador { get; set; }
+
+        public DateOnly Data { get; set; }
+
+        public string DataReferencia { get; set; }
+
+        public double Media { get; set; }
+
+        public double Mediana { get; set; }
+        public double DesvioPadrao { get; set; }
+
+        public double Minimo { get; set; }
+
+        public double Maximo { get; set; }
+
+        public int NumeroRespondentes { get; set; }
+
+        public int BaseCalculo { get; set; }
+    }
+}
---
+++ ViewModels/ExpectativaMercadoMensalViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Polo_Projeto_WPF.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
+namespace Polo_Projeto_WPF.ViewModels
+{
+    public class ExpectativaMercadoMensalViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<string> IndicadoresSelect { get; }
+        public ObservableCollection<ExpectativaMercadoMensal> Expectativa { get; set; }
+
+        
+        private string _cmbIndicador;
+        public string CmbIndicador
+        {
+            get { return _cmbIndicador; }
+            set
+            {
+                _cmbIndicador = value;
+                NotifyPropertyChanged(CmbIndicador);
+            }
+        }
+
+
+        private DateTime _dtpDataInicial = DateTime.Now;
+
+        public DateTime DtpDataInicial
+        {
+            get { return _dtpDataInicial; }
+            set
+            {
+                _dtpDataInicial = value;
+                NotifyPropertyChanged(DtpDataInicial.ToString());
+            }
+        }
+
+        private DateTime _dtpDataFinal = DateTime.Now;
+
+        public DateTime DtpDataFinal
+        {
+            get { return _dtpDataFinal; }
+            set
+            {
+                _dtpDataFinal = value;
+                NotifyPropertyChanged(_dtpDataFinal.ToString());
+            }
+        }
+
+        public ExpectativaMercadoMensalViewModel()
+        {
+
+           IndicadoresSelect = new ObservableCollection<string> { "IPCA", "Câmbio", "Selic" };
+
+        }
+
+
+        public void NotifyPropertyChanged(string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
