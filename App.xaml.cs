47e4bc7cd30f49b1ca28327faa9fc8d2cae27db1
augustorib <augustorifra@gmail.com>
2024-06-26 23:53:46 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Polo_Projeto_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
